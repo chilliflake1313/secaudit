62816a0fd1d0d65f1caf79a5d2084c71a96d1159
Ram <163444056+chilliflake1313@users.noreply.github.com>
2026-05-03 12:48:36 +0530

    Worker: connect to MongoDB before processing jobs

--- backend/src/worker/scan.worker.js
+++ backend/src/worker/scan.worker.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const { Worker } = require("bullmq");
 const { connection } = require("../queue/scan.queue");
 const { runScan } = require("../services/scan.service");
 
-new Worker(
-  "scan",
-  async (job) => {
-    await runScan(job.data.scanId);
-  },
-  { connection }
-);
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { maxPoolSize: 5 });
+  } catch (err) {
+    console.error('Worker Mongo connect error', err);
+    process.exit(1);
+  }
+
+  new Worker(
+    "scan",
+    async (job) => {
+      await runScan(job.data.scanId);
+    },
+    { connection }
+  );
+})();
